9ccfec94fff77a8bd9013307bf192ea2e1574fca
Edwin Serna <desarrolladorjr5.spv@gmail.com>
2021-03-13 14:26:16 -0500

    :bulb: Documenting source code: Se añadió la documentación inicial con Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Swagger Documentation API
+  const config = new DocumentBuilder()
+    .setTitle('Free Deail')
+    .setDescription('Esta es una API para la logistica de domicilios, ventas y demás.')
+    .setVersion('1.0')
+    .addTag('delivery, free deal, foods, merchant products, market free')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
